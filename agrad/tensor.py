5419d0fe3afd25dda092f6175efa39a5e1aa44e9
Stefan Wong <stfnwong@gmail.com>
2023-08-25 17:19:33 +0100

    Forward method on Tensor

--- agrad/tensor.py
+++ agrad/tensor.py
@@ -2,6 +2,7 @@
 
 from typing import Any, Optional, Self
 from collections import namedtuple
+import numpy as np
 
 
 Children = namedtuple("Children", ["a", "b", "op"])
@@ -9,12 +10,39 @@ Children = namedtuple("Children", ["a", "b", "op"])
 
 
 class Tensor:
-    def __init__(self, value:Optional[Any]=None):
+    def __init__(
+        self,
+        value: Optional[Any]=None,
+        children: Optional[Children]=None,
+    ):
         self.value = value
+        self.children = children
 
     def __repr__(self) -> str:
         return f"T:{self.value}"
 
     def __add__(self, other: Self) -> Self:
-        t = Tensor(value=self.value + other.value)
+        c = Children(self, other, np.add)
+        t = Tensor(children=c)
         return t
+
+    def __mul__(self, other: Self) -> Self:
+        c = Children(self, other, np.multiply)
+        t = Tensor(children=c)
+
+        return t.forward()
+
+    def forward(self) -> Self:
+        # Check if this is a leaf node
+        if self.children is None:
+            return self
+
+        # Compute forward pass of children in tree
+        a = self.children.a.forward()
+        b = self.children.b.forward()
+
+        # If values are set then compute the real value of this Tensor
+        if a.value is not None and b.value is not None:
+            self.value = self.children.op(a.value, b.value)
+
+        return self
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -6,4 +6,17 @@ def test_tensor_add():
     b = Tensor(4)
 
     c = a + b
+    c.forward()
     assert c.value == 7
+
+
+
+def test_tensor_add_children():
+    a = Tensor(3)
+    b = Tensor(4)
+
+    z1 = a + b
+    z2 = z1 * b
+
+    z2.forward()     # need to call forward() to compute values in leaf nodes
+    assert z2.value == 28  # (3 + 4) * 4
